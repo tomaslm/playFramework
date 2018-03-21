85043ad95d798e91a30c42fb88dc7b1701e5e90e
Tomas Labella Menito <tomaslm@hotmail.com>
2018-03-21 18:02:15 -0300

    Implementation of query with filters

---
+++ produtos-api/app/models/EnvelopeDeProdutos.java
@@ -0,0 +1,15 @@
+package models;
+
+import java.util.List;
+
+public class EnvelopeDeProdutos {
+	private List<Produto> produtos;
+
+	public EnvelopeDeProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
+
+	public List<Produto> getProdutos() {
+		return this.produtos;
+	}
+}
